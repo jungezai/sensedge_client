d5e27e85813e11b3debe6729455284b85c837fe9
Zhu Yi <chuyee@vegaltair.com>
2017-11-12 16:28:38 -0800

    update installation for Raspberry Pi Jessie and Stretch

--- run.sh
+++ run.sh
@@ -8,21 +8,23 @@ if [ "$1" = "install" ]; then
 			if [ ${version} -lt 9 ]; then
 				# update node to v7.x
 				curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-				sudo apt install nodejs
 				# install mosquitto
 				wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
 				sudo apt-key add mosquitto-repo.gpg.key
 				rm mosquitto-repo.gpg.key
 				sudo wget http://repo.mosquitto.org/debian/mosquitto-jessie.list
 				sudo mv mosquitto-jessie.list /etc/apt/sources.list.d/
-				# patch noble with bluez hci workaround
-				cd node_modules
-				rm -rf bluetooth-hci-socket
-				npm install sandeepmistry/node-bluetooth-hci-socket#rework-kernel-workarounds
+			else
+				# update node to v8.x
+				curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 			fi
 			sudo apt-get update
-			sudo apt install mosquitto
-			sudo apt install mosquitto-clients
+			sudo apt install nodejs
+			sudo apt install mosquitto mosquitto-clients
+			npm install
+			# patch noble with bluez hci workaround
+			rm -rf node_modules/bluetooth-hci-socket
+			npm install sandeepmistry/node-bluetooth-hci-socket#rework-kernel-workarounds
 			# access BT without root
 			sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
 			sudo setcap cap_net_raw,cap_net_admin+eip $(eval readlink -f `which hciconfig`)
@@ -30,8 +32,9 @@ if [ "$1" = "install" ]; then
 			sudo cp scripts/sensedge /etc/init.d/
 			sudo systemctl enable sensedge
 		fi
+	else
+		npm install
 	fi
-	npm install noble nodemailer ip mysql
 	exit 0
 fi
 if [ "$1" = "sim" ]; then
